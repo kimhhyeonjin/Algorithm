bdef3ed8eef0852be5a5cb9b7d88ace0d8cee0ab
kimhhyeonjin <101795292+kimhhyeonjin@users.noreply.github.com>
2023-08-23 12:08:41 +0900

    [Silver I] Title: 회의실 배정, Time: 312 ms, Memory: 59204 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,17 +1,21 @@
-N = int(input())
-arr = [list(map(int, input().split())) for _ in range(N)]
-# arr을 시작하는 시간을 기준으로 오름차순 정렬
-arr.sort(key=lambda x:x[0])
-# arr을 끝나는 시간을 기준으로 오름차순 정렬
-arr.sort(key=lambda x: x[1])
-# print(arr)
+import sys
+
+N = int(sys.stdin.readline().strip())
+time = []
+for _ in range(N):
+    time.append(list(map(int, sys.stdin.readline().strip().split())))
+
+# 종료시간이 빠른 순서로 정렬
+# 종료시간이 동일한 경우 시작시간이 빠른 순서로 정렬
+time.sort(key=lambda x: (x[1], x[0]))
+# print(time)
 
 cnt = 0
-compare = 0
+# 종료시간
+l = 0
 for i in range(N):
-    if compare <= arr[i][0]:
-        compare = arr[i][1]
+    # 시작시간이 이전 종료시간보다 크거나 같으면
+    if time[i][0] >= l:
         cnt += 1
-    else:
-        continue
+        l = time[i][1]
 print(cnt)
