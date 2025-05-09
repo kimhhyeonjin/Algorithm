5f05f2c4582f996d87a3452cd71ba1c9672da7e2
kimhhyeonjin <101795292+kimhhyeonjin@users.noreply.github.com>
2025-05-09 23:42:07 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,19 +1,16 @@
--- 코드를 입력하세요
 SELECT M.MEMBER_NAME, R.REVIEW_TEXT, DATE_FORMAT(R.REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
 FROM MEMBER_PROFILE AS M JOIN REST_REVIEW AS R ON M.MEMBER_ID = R.MEMBER_ID
-WHERE M.MEMBER_ID IN (
+WHERE R.MEMBER_ID IN (
     SELECT MEMBER_ID
     FROM REST_REVIEW
     GROUP BY MEMBER_ID
-    HAVING COUNT(REVIEW_ID) = (
-        SELECT MAX(RI)
+    HAVING COUNT(*) = (
+        SELECT MAX(REVIEW_COUNT)
         FROM (
-            -- AS RI를 해줘야 SELECT절에서 MAX를 정확하게 구할 수 있음
-            SELECT COUNT(REVIEW_ID) AS RI
+            SELECT COUNT(*) AS REVIEW_COUNT
             FROM REST_REVIEW
             GROUP BY MEMBER_ID
-            -- AS C를 입력하지 않으면 'Every derived table must have its own alias' 에러 발생
-            ) AS C
+        ) AS RC
     )
 )
 ORDER BY REVIEW_DATE, REVIEW_TEXT
