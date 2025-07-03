850d24c90067e4ee910547eab3e5189ada5aa4f6
kimhhyeonjin <101795292+kimhhyeonjin@users.noreply.github.com>
2025-07-03 21:50:46 +0900

    [level 3] Title: 이중우선순위큐, Time: 94.83 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -3,7 +3,6 @@ import heapq
 def solution(operations):
     min_heap = []
     max_heap = []
-    removed = set()
     count = {}
 
     for op in operations:
@@ -20,27 +19,25 @@ def solution(operations):
             
         elif command[0] == "D":
             if command[1] == "1":
-                while max_heap and (-max_heap[0] in removed or count[-max_heap[0]] == 0):
+                while max_heap and count[-max_heap[0]] == 0:
                     popped_val = -heapq.heappop(max_heap)
-                    removed.add(popped_val)
                 
                 if max_heap:
                     max_val = -heapq.heappop(max_heap)
                     count[max_val] -= 1
                     
             elif command[1] == "-1":
-                while min_heap and (min_heap[0] in removed or count[min_heap[0]] == 0):
+                while min_heap and count[min_heap[0]] == 0:
                     popped_val = heapq.heappop(min_heap)
-                    removed.add(popped_val)
                 
                 if min_heap:
                     min_val = heapq.heappop(min_heap)
                     count[min_val] -= 1
 
-    while min_heap and (min_heap[0] in removed or count[min_heap[0]] == 0):
+    while min_heap and count[min_heap[0]] == 0:
         heapq.heappop(min_heap)
         
-    while max_heap and (-max_heap[0] in removed or count[-max_heap[0]] == 0):
+    while max_heap and count[-max_heap[0]] == 0:
         heapq.heappop(max_heap)
 
     if not min_heap or not max_heap:
