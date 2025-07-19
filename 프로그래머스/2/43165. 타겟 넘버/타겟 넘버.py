a67f70838df6039a5daf28e18793532da0c82c49
kimhhyeonjin <101795292+kimhhyeonjin@users.noreply.github.com>
2025-07-19 17:38:17 +0900

    [level 2] Title: 타겟 넘버, Time: 389.69 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,22 +1,18 @@
+answer = 0
+
+def dfs(num, target, d, depth, numbers):
+    global answer
+    
+    if d == depth:
+        if num == target:
+            answer += 1
+        return
+    else:
+        dfs(num + numbers[d], target, d + 1, depth, numbers)
+        dfs(num - numbers[d], target, d + 1, depth, numbers)
+
 def solution(numbers, target):
-    answer = 0
-    visited = [0] * len(numbers)
-    def dfs(i, val):
-        # global과 nonlocal
-        # global은 일반 함수 내에서 전역 변수를 대상으로 함
-        # nonlocal은 중첩 함수 내에서 비지역 변수를 대상으로 함
-        # 아래의 경우 nonlocal이 아닌 global answer으로 하면
-        # NameError 발생
-        nonlocal answer
-        if i == len(numbers):
-            if val == target:
-                answer += 1
-            return
-        if visited[i] == 0:
-            visited[i] += 1
-            dfs(i+1, val + numbers[i])
-        if visited[i] == 1:
-            visited[i] -= 1
-            dfs(i+1, val - numbers[i])
-    dfs(0, 0)
+    global answer
+    dfs(0, target, 0, len(numbers), numbers)
+
     return answer
